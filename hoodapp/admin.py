6aa7eb31513ce20d85ab65def46df24a60c25aaf
finaclemence15 <finaclemence15@gmail.com>
2019-11-04 10:09:35 +0200

    testing profile model

--- hoodapp/admin.py
+++ hoodapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Comment, Business, Post,Neighbourhood, Profile
+
 
 # Register your models here.
+admin.site.register(Comment)
+admin.site.register(Business)
+admin.site.register(Post)
+admin.site.register(Neighbourhood)
+admin.site.register(Profile)
