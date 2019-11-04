ca47158a92c77c9f41bb235b5d931b1274cc38b8
finaclemence15 <finaclemence15@gmail.com>
2019-11-04 09:51:20 +0200

    testing class model

--- hoodapp/tests.py
+++ hoodapp/tests.py
@@ -3,16 +3,38 @@ from . models import Comment, Business, Post,Neighbourhood, Profile
 
 # Create your tests here.
 
-
-# Create your tests here.
-
-class ProfileTestClass(TestCase):
-    # Set up method
+# Profile model test  
+      
+class ProfileTestClass(TestCase):        
+    
+        # Set up method
     def setUp(self):
-        self.user = User.objects.create_user(username='thoni', password='qwertyuiop')
-        self.profile = Profile(id=1, name='Tom Thomassen',user = self.user,bio='Bionic')
-    # Test instance
+        self.image= Profile(profile_pict = 'img.jpg', bio ='image', email = "fi@gmail.com",phone_number = '07854222')
+        
+        # Testing  instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.profile,Profile))
+        self.assertTrue(isinstance(self.image,Profile))     
 
+        # Testing Save Method of Profile model
+    def test_save_method(self):
+        self.image.save_profile()
+        images = Profile.objects.all()
+        self.assertTrue(len(images) > 0)   
+        
+    # Testing  delete method of Profile model     
+    def test_delete(self):
+        self.image= Profile(profile_pict = 'img.jpg', bio ='image')
+        self.image.save_profile()
+        image = Profile.objects.filter(profile_pict = 'img.jpg').first()
+        delete = Profile.objects.filter(id = image.id).delete()
+        images = Profile.objects.all()
+        self.assertTrue(len(images) == 0)         
+
+    # Testing  update method of Profile model    
+    def test_update(self):
+        self.image.save_profile()
+        image = Profile.objects.filter(profile_pict = 'img.jpg').first()
+        update = Profile.objects.filter(id = image.id).update(profile_pict = 'cake.jpg')
+        updated = Profile.objects.filter(profile_pict = 'cake.jpg').first()
+        self.assertNotEqual(image.profile_pict, updated.profile_pict)      
 
